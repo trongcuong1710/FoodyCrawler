e5a77ebafc789892bd8fff883e08265eedbcbe53
Cuong Doan <trongcuong1710@gmail.com>
2017-10-17 12:46:09 +0700

    Initial Commit

---
+++ FoodyCrawler/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FoodyCrawler
+//
+//  Created by Cole Doan on 10/17/17.
+//  Copyright © 2017 Trong Cuong Doan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ FoodyCrawler/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FoodyCrawler
+//
+//  Created by Cole Doan on 10/17/17.
+//  Copyright © 2017 Trong Cuong Doan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
